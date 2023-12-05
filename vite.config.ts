a6fd5ca484fcb82d5e959dd5326e98d5c7394aee
Brandon Roberts <robertsbt@gmail.com>
2023-12-04 22:00:57 -0600

    fix: add build support for service worker through plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
+import { Plugin, defineConfig } from 'vite';
 import analog from '@analogjs/platform';
+import { augmentAppWithServiceWorker } from '@angular-devkit/build-angular/src/utils/service-worker';
+import * as path from 'path';
+import { UserConfig } from 'vite';
+
+function swBuildPlugin(): Plugin {
+  let config: UserConfig;
+  return {
+    name: 'analog-sw',
+    config(_config) {
+      config = _config;
+    },
+    async closeBundle() {
+      if (config.build?.ssr) {
+        return;
+      }
+      console.log('Building service worker');
+      await augmentAppWithServiceWorker('.', process.cwd(), path.join(process.cwd(), 'dist/client'), '/');
+    }
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,7 +32,10 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [
+    analog(),
+    swBuildPlugin()
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
